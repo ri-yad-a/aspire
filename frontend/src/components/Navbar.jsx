e70e5d079efcf10af69549b2efe04478b58dd1ba
ri-yad-a <riyad.abdullayev@ucalgary.ca>
2023-11-22 14:58:15 -0700

    Made login and signup mobile friendly

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import "../styles/Navbar.css";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <nav>
         <ul>
-          <li>Applications</li>
-          <li>Jobs</li>
-          <li>Interviews</li>
+          <li><Link to="/applications">Applications</Link></li>
+          <li><Link to="/jobs">Jobs</Link></li>
+          <li><p>ASPIRE</p></li>
+          <li><Link to="/interviews">Interviews</Link></li>
+          <li>Profile</li>
         </ul>
       </nav>
     </div>
