5e7fa4f4f68a2ccc975d7ed3ac15bbaca32dc3d6
ri-yad-a <riyad.abdullayev@ucalgary.ca>
2023-11-29 20:25:20 -0700

    Login and Signup functionality with backend and database integration in progress

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,26 @@
 import express from "express";
+import postRoutes from "./routes/posts.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/posts.js";
 
 const app = express();
+
+app.use(express.json());
+app.use("/api/posts", postRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req,res)=>{
+    res.json("Hello, this is the backend");
+})
+
+app.get("/users", (req,res)=>{
+    const q = "SELECT * FROM aspiredb.users"; 
+    db.query(q,(err, data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    });
+})
 
 app.listen(8800, ()=> {
     console.log("Connected to backend");
