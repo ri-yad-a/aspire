e80a9a315f27177243f10b927075177074ca6853
ri-yad-a <riyad.abdullayev@ucalgary.ca>
2023-12-11 15:33:50 -0700

    Added logout button and finished backend for interviews page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,54 @@
-import React from 'react';
+import React from "react";
 import "../styles/Navbar.css";
-import { Link } from 'react-router-dom';
+import { useContext, useState   } from "react";
+import { AuthContext } from "../context/authContext";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+
+  const {logout} = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const [err, setError] = useState(null);
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    try {
+      await logout();
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
+
   return (
     <header>
-      <nav className='navbar'>
-        <div className='navbar-logo'>ASPIRE</div>
-        <ul className='nav-links'>
-          <li><Link to="/applications">Applications</Link></li>
-          <li><Link to="/jobs">Jobs</Link></li>
+      <nav className="navbar">
+        <div className="navbar-logo">ASPIRE</div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/applications">Applications</Link>
+          </li>
+          <li>
+            <Link to="/jobs">Jobs</Link>
+          </li>
           {/* <li><Link to="/applications">ASPIRE</Link></li> */}
-          <li><Link to="/interviews">Interviews</Link></li>
+          <li>
+            <Link to="/interviews">Interviews</Link>
+          </li>
           {/* <li><Link to="/profile">Profile</Link></li> */}
         </ul>
-        <div className='navbar-profile'><Link to="/profile">Profile</Link></div>
+        <div className="navbar-profile">
+          <div>
+            <Link to="/profile">Profile</Link>
+          </div>
+          <div onClick={handleLogout}>Logout</div>
+        </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Navbar;
