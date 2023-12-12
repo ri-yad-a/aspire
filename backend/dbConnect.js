d97079a599681c947286aa1b61e0d3747e2a67ac
louiskunstmann <louis.kunstmann@web.de>
2023-12-12 07:19:44 -0700

    Added .env file

--- backend/dbConnect.js
+++ backend/dbConnect.js
@@ -1,10 +1,24 @@
-import mysql from "mysql";
+// import mysql from "mysql";
 
+
+// export const db = mysql.createConnection({
+//     //host:"localhost",
+//     user:"root",
+//     password:"Aspire@2023!",
+//     database:"aspiredb",
+//     port:3306
+// });
+
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 export const db = mysql.createConnection({
-    //host:"localhost",
-    user:"root",
-    password:"Aspire@2023!",
-    database:"aspiredb",
-    port:3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 });
